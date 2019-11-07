5626dabfc4dc3f7810c41f2ce4f0b64b6c786469
gustin <gustin@users.noreply.github.com>
2019-11-06 17:03:07 -0800

    Move storage into more generic function for general use.

--- src/storage.rs
+++ src/storage.rs
@@ -1,23 +1,29 @@
 use rocksdb::*;
 use std::path::{Path, PathBuf};
+use std::str;
 
-fn store_some_things() {
+fn store(key, value) {
     let path = Path::new("_plaintext");
     let db = DB::open_default(&path).unwrap();
 
-    db.put(b"user_id_1", b"secret_1");
-    db.put(b"user_id_1", b"secret_2");
-
-    //   let r: Result<Option<DBVector>, Error> = db.get(b"user_id_1");
-
-    let mut iter = db.iterator(IteratorMode::From(b"user_id_1", Direction::Forward));
-    for (key, value) in iter {
-        println!("Saw {:?} {:?}", key, value);
-    }
-    db.delete(b"k1");
+    db.put(key, value);
 }
+
 
 #[test]
 fn test_storing_data() {
-    store_some_things();
+    store(b"user_id_1", b"secret_1");
+
+    let db = DB::open_default(&path).unwrap();
+
+    let mut iter = db.iterator(IteratorMode::From(b"user_id_1", Direction::Forward));
+    for (key, value) in iter {
+        println!(
+            "=> {:?}:{:?}",
+            str::from_utf8(&key).unwrap(),
+            str::from_utf8(&value).unwrap()
+        );
+    }
+
+    db.delete(b"user_id_1");
 }
