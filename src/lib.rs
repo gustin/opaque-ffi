555661f8f3f1a4f601e8666b2f69d80bd82ff689
gustin <gustin@users.noreply.github.com>
2019-12-06 12:55:12 -0500

    Change to newer cdylib, handle c strings as argument.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
-use std::str;
 
 mod authenticators;
 //mod storage;
+use libc::{c_char};
+use std::ffi::CStr;
+use std::str;
 
 #[no_mangle]
-pub extern "C" fn generate_totp(user_id: &str) -> String {
-    let totp = authenticators::generate_totp(user_id);
+pub extern fn generate_totp(user_id: *const c_char) {
+    let c_str = unsafe {
+        assert!(!user_id.is_null());
+        CStr::from_ptr(user_id)
+    };
 
-    return "Holla".to_string();
+    let user_id = c_str.to_str().unwrap();
+    let totp = authenticators::generate_totp(user_id);
 }
