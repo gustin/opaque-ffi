e5483c6030060f55e16caa50fe852bd20633d281
gustin <gustin@users.noreply.github.com>
2019-12-09 16:25:06 -0500

    Pass a registration struct back with arrays of bytes.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod authenticators;
 mod opaque;
 //mod storage;
 use libc::c_char;
+use std::convert::From;
 use std::ffi::{CStr, CString};
 use std::slice;
 use std::str;
@@ -32,11 +33,18 @@ pub extern "C" fn free_totp_qr(qr: *mut c_char) {
 
 // OPAQUE interface
 
+#[repr(C)]
+pub struct Registration {
+    beta: *const u8,
+    v: *const u8,
+    pub_s: *const u8,
+}
+
 #[no_mangle]
 pub extern "C" fn registration_1(
     username: *const c_char,
     alpha: *const u8,
-) {
+) -> Registration {
     let username_c_str = unsafe {
         assert!(!username.is_null());
         CStr::from_ptr(username)
@@ -47,5 +55,15 @@ pub extern "C" fn registration_1(
     let mut alpha: [u8; 32] = [0; 32];
     alpha.copy_from_slice(&defrag[..32]);
 
-    opaque::registration_init(username, &alpha);
+    let (beta, v, pub_s) = opaque::registration_init(username, &alpha);
+    let beta = Box::new(beta);
+    let v = Box::new(v);
+    let pub_s = Box::new(pub_s);
+
+    return Registration {
+        beta: Box::into_raw(beta) as *mut u8,
+        v: Box::into_raw(v) as *mut u8,
+        pub_s: Box::into_raw(pub_s) as *mut u8,
+    }
 }
+
--- src/opaque.rs
+++ src/opaque.rs
@@ -1,13 +1,11 @@
-
-
-pub fn registration_init(username: &str, alpha: &[u8; 32]) {
+pub fn registration_init(
+    username: &str,
+    alpha: &[u8; 32],
+) -> ([u8; 32], [u8; 32], [u8; 32]) {
     // opaque client code, call function in lib for now
     // client_registration_values
     // then package and post to a url
 
-
     // => Registration 1
-    let (beta, v, pub_s) = opaque::registration_1(username, &alpha);
+    return opaque::registration_1(username, &alpha);
 }
-
-
