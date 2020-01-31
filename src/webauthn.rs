41e538c018003ac9639cecf6e614b40a8fafa18d
gustin <gustin@users.noreply.github.com>
2020-01-31 12:18:40 -0500

    Add copyright to webauthn module.

--- src/webauthn.rs
+++ src/webauthn.rs
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2019 Plaintext, LLC - All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ */
+
 use serde::{Deserialize, Serialize};
 use webauthn_rs::ephemeral::WebauthnEphemeralConfig;
 use webauthn_rs::error::WebauthnError;
